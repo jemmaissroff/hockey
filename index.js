639dcbbd9ff4aeae6cc591885ecd1318f6b578d8
Jemma Issroff <jemmaissroff@gmail.com>
2020-11-24 11:49:36 -0500

    testing graphene

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var neo4j = require('neo4j-driver');
+
+var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+
+var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass), {encrypted: 'ENCRYPTION_ON'});
+
+var session = driver.session();
+
+session
+  .run("MATCH (:Person {name: 'Tom Hanks'})-[:ACTED_IN]->(movies) RETURN movies.title AS title")
+  .subscribe({
+    onNext: function(record) {
+      console.log(record.get('title'));
+    },
+    onCompleted: function() {
+        session.close();
+    },
+    onError: function() {
+        console.log(error);
+    }
+  });
